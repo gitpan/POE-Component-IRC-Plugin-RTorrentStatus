ce0fd963d2fac71d98d55315d51673070228e195
Hinrik Örn Sigurðsson <hinrik.sig@gmail.com>
2010-04-09 19:43:12 +0000

    Import of HINRIK/POE-Component-IRC-Plugin-RTorrentStatus-0.04 from CPAN.
    gitpan-cpan-distribution: POE-Component-IRC-Plugin-RTorrentStatus
    gitpan-cpan-version:      0.04
    gitpan-cpan-path:         HINRIK/POE-Component-IRC-Plugin-RTorrentStatus-0.04.tar.gz
    gitpan-cpan-author:       HINRIK
    gitpan-cpan-maturity:     released

--- lib/POE/Component/IRC/Plugin/RTorrentStatus.pm
+++ lib/POE/Component/IRC/Plugin/RTorrentStatus.pm
@@ -12,18 +12,24 @@ use POE::Component::IRC::Plugin qw(PCI_EAT_NONE);
 use POE::Component::IRC::Common qw(NORMAL DARK_GREEN DARK_BLUE ORANGE TEAL BROWN);
 use POE::Component::IRC::Plugin::FollowTail;
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 sub new {
     my ($package, %args) = @_;
     my $self = bless \%args, $package;
 
-    if (!defined $self->{Torrent_log} || !-e $self->{Torrent_log}) {
-        croak __PACKAGE__ . ": the specified torrent log doesn't exist";
+    if (!defined $self->{Torrent_log}) {
+        croak __PACKAGE__ . ": No torrent log file defined";
     }
 
     if (ref $self->{Channels} ne 'ARRAY' || !$self->{Channels}) {
         croak __PACKAGE__ . ': No channels defined';
+    }
+
+    if (!-e $self->{Torrent_log}) {
+        open my $foo, '>', $self->{Torrent_log}
+            or die "Can't create $self->{Torrent_log}: $!\n";
+        close $foo;
     }
 
     # defaults
